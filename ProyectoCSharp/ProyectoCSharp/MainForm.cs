dc540bba4d92989a4e0d37f63e5eb02d50847264
KarollJC <juck080419hgtrvra0@cbtis217.edu.mx>
2024-10-24 19:14:37 -0600

    Se agregaron comentarios

--- ProyectoCSharp/ProyectoCSharp/MainForm.cs
+++ ProyectoCSharp/ProyectoCSharp/MainForm.cs
@@ -32,15 +32,17 @@ namespace ProyectoCSharp
 		
 		void Button1Click(object sender, EventArgs e)
 		{
-			
+			//Para seleccionar los 2 ultimos digitos del año y mes
 			int year = mcNacimiento.SelectionStart.Year;
 			int month = mcNacimiento.SelectionStart.Month;
 			
 			string UltimosDosY = (year % 100).ToString("D2");
 			string UltimosDosM = month.ToString("D2");
 			
+			//Para seleccionar los 2 primeros digitos del apellido 1, 1 del 2 y 1 del nombre
 			if(txtMaterno.Text.Length >= 2 && txtPaterno.Text.Length >= 1 && txtName.Text.Length >= 1)
 			{
+				//Se agregan al label los datos
 				lblRFC.Text = txtPaterno.Text.Substring(0, 2) + txtMaterno.Text.Substring(0, 1)
 				+ txtName.Text.Substring(0, 1) + UltimosDosY + UltimosDosM + mcNacimiento.SelectionStart.Day.ToString();
 			}
