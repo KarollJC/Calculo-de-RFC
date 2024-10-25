2d6a076ecdb0424d86ea2cd1764d02e00daaa198
KarollJC <juck080419hgtrvra0@cbtis217.edu.mx>
2024-10-24 18:52:20 -0600

    Calcular RFC

--- ProyectoCSharp/ProyectoCSharp/MainForm.cs
+++ ProyectoCSharp/ProyectoCSharp/MainForm.cs
@@ -30,7 +30,20 @@ namespace ProyectoCSharp
 			//
 		}
 		
-		
+		void Button1Click(object sender, EventArgs e)
+		{
+			
+			int year = mcNacimiento.SelectionStart.Year;
+			int month = mcNacimiento.SelectionStart.Month;
+			
+			string UltimosDosY = (year % 100).ToString("D2");
+			string UltimosDosM = month.ToString("D2");
+			
+			if(txtMaterno.Text.Length >= 2 && txtPaterno.Text.Length >= 1 && txtName.Text.Length >= 1)
+			{
+				lblRFC.Text = txtPaterno.Text.Substring(0, 2) + txtMaterno.Text.Substring(0, 1)
+				+ txtName.Text.Substring(0, 1) + UltimosDosY + UltimosDosM + mcNacimiento.SelectionStart.Day.ToString();
+			}
 		}
 	}
 }
